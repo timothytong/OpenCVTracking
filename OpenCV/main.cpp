c595661fff6f7573eb3bdbd8e28a84604ef922a1
timothytong <timothy.tong@hotmail.ca>
2016-01-16 00:13:58 -0500

    Added code to find edges with Canny algo

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -5,13 +5,13 @@
 #include "opencv2/objdetect/objdetect.hpp"
 
 using namespace cv;
+using namespace std;
 
 const String CONTROL_WINDOW_NAME = "Controls";
 
 // Define Hue, Saturation, and Value values for the sliders (trackbars)
-const int H_MAX = 256;
-const int S_MAX = 256;
-const int V_MAX = 256;
+const int THRESHOLD_MAX = 256;
+RNG rng(12345);
 
 // Define Hue, Saturation, and Value values for the sliders (trackbars)
 int h_threshold_min = 0, h_threshold_max = 256;
@@ -22,20 +22,22 @@ static void on_trackbar( int, void* ){} // Don't need to do much here yet...
 
 void make_UI(){
     namedWindow(CONTROL_WINDOW_NAME);
+
+    createTrackbar("Min Hue", CONTROL_WINDOW_NAME, &h_threshold_min, THRESHOLD_MAX, on_trackbar);
+    createTrackbar("Max Hue", CONTROL_WINDOW_NAME, &h_threshold_max, THRESHOLD_MAX, on_trackbar);
     
-    createTrackbar("Min Hue", CONTROL_WINDOW_NAME, &h_threshold_min, H_MAX, on_trackbar);
-    createTrackbar("Max Hue", CONTROL_WINDOW_NAME, &h_threshold_max, H_MAX, on_trackbar);
+    createTrackbar("Min Saturation", CONTROL_WINDOW_NAME, &s_threshold_min, THRESHOLD_MAX, on_trackbar);
+    createTrackbar("Max Saturation", CONTROL_WINDOW_NAME, &s_threshold_max, THRESHOLD_MAX, on_trackbar);
     
-    createTrackbar("Min Saturation", CONTROL_WINDOW_NAME, &s_threshold_min, S_MAX, on_trackbar);
-    createTrackbar("Max Saturation", CONTROL_WINDOW_NAME, &s_threshold_max, S_MAX, on_trackbar);
-    
-    createTrackbar("Min Value", CONTROL_WINDOW_NAME, &v_threshold_min, V_MAX, on_trackbar);
-    createTrackbar("Max Value", CONTROL_WINDOW_NAME, &v_threshold_max, V_MAX, on_trackbar);
+    createTrackbar("Min Value", CONTROL_WINDOW_NAME, &v_threshold_min, THRESHOLD_MAX, on_trackbar);
+    createTrackbar("Max Value", CONTROL_WINDOW_NAME, &v_threshold_max, THRESHOLD_MAX, on_trackbar);
     
 }
 
 int main(int argc, char* argv[]){
     VideoCapture capture(0);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     Mat Webcam;
     Mat HSVcam;
     Mat thresholdCam;
@@ -55,22 +57,59 @@ int main(int argc, char* argv[]){
         printf("v_max: %d\n", v_threshold_max);
         printf("v_min: %d\n", v_threshold_min);
         
-        
+        /* for sliders */
+
+        /*
          inRange(HSVcam,Scalar(h_threshold_min, s_threshold_min, v_threshold_min),
          Scalar(h_threshold_max, s_threshold_max, v_threshold_max),
          thresholdCam);
+         */
+        
+        inRange(HSVcam,Scalar(145, 128, 66),
+                Scalar(256, 243, 256),
+                thresholdCam);
         
         
+        /* for drawn red box */
         /*
         inRange(HSVcam,Scalar(0, 101, 95),
                 Scalar(256, 180, 139),
                 thresholdCam);
         */
         
+        /* for red pen */
+        /*
+        inRange(HSVcam,Scalar(130, 58, 30),
+                Scalar(185, 256, 111),
+                thresholdCam);
+         */
         
-        imshow("HSV", HSVcam);
-        imshow("RAW", Webcam);
-        imshow("Binary threshold", thresholdCam);
+        Mat erosionKernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+        erode(thresholdCam, thresholdCam, erosionKernel);
+        
+        
+        Mat dilationKernel = getStructuringElement(MORPH_RECT, Size(8, 8));
+        dilate(thresholdCam, thresholdCam, dilationKernel);
+        
+        imshow("Binary threshold (dilation)", thresholdCam);
+        
+        Mat edges;
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        
+        // Detect edges using canny
+        cv::Canny(thresholdCam, edges, 100, 200);
+        
+        findContours(thresholdCam, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+        
+        Mat drawing = Mat::zeros( edges.size(), CV_8UC3 );
+        for( int i = 0; i< contours.size(); i++ )
+        {
+            Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255) );
+            drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+            
+        }
+
         
         // Lower the refresh rate so it doesn't die...
         waitKey(15);
